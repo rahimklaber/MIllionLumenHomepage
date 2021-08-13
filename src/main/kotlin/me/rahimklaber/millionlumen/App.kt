f0cf480122822406d6976b42ff6203c849ecb374
Rahim Klabér <r.e.p.klaber@student.tudelft.nl>
2021-08-13 06:59:45 +0200

    Add image to canvas by dragging

--- src/main/kotlin/me/rahimklaber/millionlumen/App.kt
+++ src/main/kotlin/me/rahimklaber/millionlumen/App.kt
@@ -4,6 +4,7 @@ import externals.stellar.Server
 import externals.stellar.ServerApi
 import io.kvision.Application
 import io.kvision.core.*
+import io.kvision.html.button
 import io.kvision.html.header
 import io.kvision.module
 import io.kvision.panel.root
@@ -16,7 +17,15 @@ class App : Application() {
         root("kvapp") {
             vPanel(alignItems = AlignItems.CENTER) {
                 appHeader()
-                pixelBoard(ObservableValue(false),1000, 1000)
+                val pixelBoardState = ObservableValue(PixelBoardState())
+                button("Add pic"){
+                    onClick {
+                        pixelBoardState.value = pixelBoardState.value.apply {
+                            addingImageToCanvas = true
+                        }
+                    }
+                }
+                pixelBoard(pixelBoardState,1000, 1000)
             }
 
         }.apply {
--- src/main/kotlin/me/rahimklaber/millionlumen/PixelBoard.kt
+++ src/main/kotlin/me/rahimklaber/millionlumen/PixelBoard.kt
@@ -3,18 +3,16 @@ package me.rahimklaber.millionlumen
 import externals.stellar.ServerApi
 import io.kvision.core.*
 import io.kvision.html.Canvas
-import io.kvision.html.button
-import io.kvision.state.ObservableState
 import io.kvision.state.ObservableValue
 import kotlinx.browser.window
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.await
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import org.w3c.dom.CanvasRenderingContext2D
-import org.w3c.dom.HTMLCanvasElement
 import org.w3c.dom.Image
+import org.w3c.files.FileReader
+import org.w3c.files.get
 
 /**
  * Class to hold info about images which need to be rendered.
@@ -33,8 +31,9 @@ class ImageInfo(
     var y: Double,
     val height: Double,
     val width: Double,
-    val hoverText: String,
-    val onClickLink: String
+    val hoverText: String = "",
+    val onClickLink: String = "",
+    val draggable: Boolean = false
 )
 
 fun CanvasRenderingContext2D.drawImage(
@@ -75,6 +74,15 @@ fun CanvasRenderingContext2D.drawImage(imageInfo: ImageInfo, initialDraw: Boolea
     )
 }
 
+
+class PixelBoardState(
+    var addingImageToCanvas: Boolean = false,
+    var imageAddedToCanvas: Boolean = false,
+    var settingImageLocation: Boolean = false,
+//    var imageUrl : String,
+    var saveImage: () -> Unit = {}
+)
+
 /**
  *  Render a canvas with the bought images.
  *
@@ -83,134 +91,200 @@ fun CanvasRenderingContext2D.drawImage(imageInfo: ImageInfo, initialDraw: Boolea
  * @param width width of the image.
  * @param height height of the image.
  */
-fun Container.pixelBoard(state: ObservableValue<Boolean>,width: Int, height: Int) = Canvas(width, height) {
-    val scope = CoroutineScope(Dispatchers.Default)
-    val imageInfos = mutableListOf<ImageInfo>()
-    val draw = { ctx: CanvasRenderingContext2D, init: Boolean ->
-        ctx.fillStyle = "#D3D3D3"
-        ctx.fillRect(0.0,0.0,width.toDouble(),height.toDouble())
-        // draw lines
-        ctx.strokeStyle = "grey"
-        for(i in 0 until 100){
-            ctx.beginPath()
-            ctx.moveTo(10.0 * i,0.0)
-            ctx.lineTo(10.0 * i, height.toDouble())
-            ctx.stroke()
+fun Container.pixelBoard(state: ObservableValue<PixelBoardState>, width: Int, height: Int) =
+    Canvas(width, height) {
+        val scope = CoroutineScope(Dispatchers.Default)
+        val imageInfos = mutableListOf<ImageInfo>()
+        val draw = { ctx: CanvasRenderingContext2D, init: Boolean ->
+            ctx.fillStyle = "#D3D3D3"
+            ctx.fillRect(0.0, 0.0, width.toDouble(), height.toDouble())
+            // draw lines
+            ctx.strokeStyle = "grey"
+            for (i in 0 until 100) {
+                ctx.beginPath()
+                ctx.moveTo(10.0 * i, 0.0)
+                ctx.lineTo(10.0 * i, height.toDouble())
+                ctx.stroke()
+            }
+            for (i in 0 until 100) {
+                ctx.beginPath()
+                ctx.moveTo(0.0, 10.0 * i)
+                ctx.lineTo(width.toDouble(), 10.0 * i)
+                ctx.stroke()
+            }
+            imageInfos.forEach { ctx.drawImage(it, init) }
         }
-        for(i in 0 until 100){
-            ctx.beginPath()
-            ctx.moveTo(0.0,10.0 * i)
-            ctx.lineTo(width.toDouble(), 10.0 * i)
-            ctx.stroke()
-        }
-        imageInfos.forEach { ctx.drawImage(it, init) }
-    }
-    onEvent {
-        //todo deal with hover text later
+        onEvent {
+            //todo deal with hover text later
 
-        mousemove = {
-            val rect = (this@Canvas.getElement() as HTMLCanvasElement).getBoundingClientRect()
-            val ctx = this@Canvas.context2D
-            val x = it.pageX - rect.left
-            val y = it.pageY - rect.top
-            ctx.clearRect(0.0, 0.0, 1000.0, 1000.0)
-            draw(ctx, false)
-            for (imageInfo in imageInfos) {
-                if (x > imageInfo.x && x < imageInfo.x + imageInfo.width && y > imageInfo.y && y < imageInfo.y + imageInfo.height) {
-                    ctx.fillText(imageInfo.hoverText, 500.0, 500.0)
-                    break
+//        mousemove = {
+//            it.preventDefault()
+//            it.stopPropagation()
+//
+//            val ctx = this@Canvas.context2D
+//            val x = it.offsetX
+//            val y = it.offsetY
+//            ctx.clearRect(0.0, 0.0, 1000.0, 1000.0)
+//            draw(ctx, false)
+//            for (imageInfo in imageInfos) {
+//                if (x > imageInfo.x && x < imageInfo.x + imageInfo.width && y > imageInfo.y && y < imageInfo.y + imageInfo.height) {
+//                    ctx.fillText(imageInfo.hoverText, 500.0, 500.0)
+//                    break
+//                }
+//            }
+//        }
+            /**
+             * Be able to drag and move image.
+             * //todo only allow this when trying to upload something.
+             */
+            var moving = false
+            var movingX = 0.0
+            var movingY = 0.0
+            mousedown = {
+                if (state.value.imageAddedToCanvas) {
+                    it.preventDefault()
+                    it.stopPropagation()
+
+                    moving = true
+                    movingX = it.offsetX
+                    movingY = it.offsetY
                 }
             }
-        }
-        /**
-         * Be able to drag and move image.
-         * //todo only allow this when trying to upload something.
-         */
-        var moving = false
-        var movingX = 0.0
-        var movingY = 0.0
-        mousedown={
-            moving = true
-            movingX = it.offsetX
-            movingY = it.offsetY
-        }
-        mouseup={
-            moving = false
-        }
-        mousemove ={
-            it.preventDefault()
-            it.stopPropagation()
+            mouseup = {
+                if (state.value.imageAddedToCanvas) {
 
-            val x = it.offsetX
-            val y = it.offsetY
-            val dx =  x - movingX
-            val dy = y - movingY
+                    it.preventDefault()
+                    it.stopPropagation()
 
-            val ctx = this@Canvas.context2D
-            if(moving) {
-                for (imageInfo in imageInfos) {
-                    if (x > imageInfo.x && x < imageInfo.x + imageInfo.width && y > imageInfo.y && y < imageInfo.y + imageInfo.height) {
-                        imageInfo.x+= dx
-                        imageInfo.y+= dy
-                        movingX = x
-                        movingY = y
-                        draw(ctx, false)
-                        break
+                    moving = false
+                }
+            }
+            mousemove = {
+                if (state.value.imageAddedToCanvas) {
+
+                    it.preventDefault()
+                    it.stopPropagation()
+
+                    val x = it.offsetX
+                    val y = it.offsetY
+                    val dx = x - movingX
+                    val dy = y - movingY
+
+                    val ctx = this@Canvas.context2D
+                    if (moving) {
+                        for (imageInfo in imageInfos) {
+                            if (imageInfo.draggable && x > imageInfo.x && x < imageInfo.x + imageInfo.width && y > imageInfo.y && y < imageInfo.y + imageInfo.height) {
+                                imageInfo.x += dx
+                                imageInfo.y += dy
+                                movingX = x
+                                movingY = y
+                                draw(ctx, false)
+                                break
+                            }
+                        }
                     }
                 }
             }
+            // Suppor for adding image by draging it.
+            dragenter = {
+                console.log(state.value.addingImageToCanvas)
+                    it.stopPropagation()
+                    it.preventDefault()
+            }
+            dragover = {
+                    it.stopPropagation()
+                    it.preventDefault()
+            }
+
+            drop = {
+                it.stopPropagation()
+                it.preventDefault()
+                if (state.value.addingImageToCanvas) {
+                    console.log("dropped")
+                    val imageFile = it.dataTransfer?.files?.get(0)
+                    imageFile?.let { file ->
+                        scope.launch {
+                            val reader = FileReader()
+                            reader.readAsDataURL(file)
+                            reader.onload = { event ->
+                                imageInfos.add(
+                                    ImageInfo(
+                                        event.target.asDynamic().result as String,
+                                        500.0,
+                                        500.0,
+                                        128.0,
+                                        128.0,
+                                        "",
+                                        "",
+                                        true
+                                    )
+                                )
+                                state.value = state.value.apply {
+                                    addingImageToCanvas = false
+                                    imageAddedToCanvas = true
+                                }
+                                draw(this@Canvas.context2D,true)
+                                null
+                            }
+
+                        }
+
+                    }
+                }
+            }
+
+
         }
-    }
-    addAfterInsertHook {
-        val ctx = this.context2D
-        scope.launch {
-            val transactions = server.transactions().forAccount(address).call().await()
-            transactions.records.forEach { tx ->
-                if (tx.memo == "millionlumen") {
-                    try {
-                        require(tx.operation_count == 2) { "operation count not 2" }
-                        val ops = tx.operations().await()
-                        val paymentOp = ops.records[0]
-                        val manageDataOp = ops.records[1]
-                        require(paymentOp.type == "payment") { "first op not payment" }
-                        require(manageDataOp.type == "manage_data") { "second op not manageData" }
-                        manageDataOp as ServerApi.ManageDataOperationRecord
-                        paymentOp as ServerApi.PaymentOperationRecord
-                        val amountPaid = paymentOp.amount
-                        val ipfsHash = manageDataOp.name
-                        val data = window.atob(manageDataOp.value.toString()).split(";")
-                        val dimensions = data[0].split("x")
-                        val width = dimensions[0].toInt()
-                        val height = dimensions[1].toInt()
-                        val x = data[1].toInt()
-                        val y = data[2].toInt()
-                        println(window.atob(manageDataOp.value.toString()))
-                        imageInfos.add(
-                            ImageInfo(
-                                "https://ipfs.io/ipfs/$ipfsHash",
-                                x.toDouble(),
-                                y.toDouble(),
-                                height.toDouble(),
-                                width.toDouble(),
-                                "yeetss",
-                                ""
+        addAfterInsertHook {
+            val ctx = this.context2D
+            scope.launch {
+                val transactions = server.transactions().forAccount(address).call().await()
+                transactions.records.forEach { tx ->
+                    if (tx.memo == "millionlumen") {
+                        try {
+                            require(tx.operation_count == 2) { "operation count not 2" }
+                            val ops = tx.operations().await()
+                            val paymentOp = ops.records[0]
+                            val manageDataOp = ops.records[1]
+                            require(paymentOp.type == "payment") { "first op not payment" }
+                            require(manageDataOp.type == "manage_data") { "second op not manageData" }
+                            manageDataOp as ServerApi.ManageDataOperationRecord
+                            paymentOp as ServerApi.PaymentOperationRecord
+                            val amountPaid = paymentOp.amount
+                            val ipfsHash = manageDataOp.name
+                            val data = window.atob(manageDataOp.value.toString()).split(";")
+                            val dimensions = data[0].split("x")
+                            val width = dimensions[0].toInt()
+                            val height = dimensions[1].toInt()
+                            val x = data[1].toInt()
+                            val y = data[2].toInt()
+                            println(window.atob(manageDataOp.value.toString()))
+                            imageInfos.add(
+                                ImageInfo(
+                                    "https://ipfs.io/ipfs/$ipfsHash",
+                                    x.toDouble(),
+                                    y.toDouble(),
+                                    height.toDouble(),
+                                    width.toDouble(),
+                                    "yeetss",
+                                    ""
+                                )
                             )
-                        )
-                    } catch (e: Exception) {
-                        println(tx.hash)
-                        println(e)
+                        } catch (e: Exception) {
+                            println(tx.hash)
+                            println(e)
+                        }
                     }
                 }
-            }
-            launch(Dispatchers.Main) {
-                println("Test")
-                draw(ctx, true)
+                launch(Dispatchers.Main) {
+                    println("Test")
+                    draw(ctx, true)
+                }
             }
         }
+    }.apply {
+        this.style {
+            border = Border(CssSize(1, UNIT.px), BorderStyle.SOLID, Color.name(Col.RED))
+        }
+        this@pixelBoard.add(this)
     }
-}.apply {
-    this.style {
-        border = Border(CssSize(1, UNIT.px), BorderStyle.SOLID, Color.name(Col.RED))
-    }
-    this@pixelBoard.add(this)
-}
